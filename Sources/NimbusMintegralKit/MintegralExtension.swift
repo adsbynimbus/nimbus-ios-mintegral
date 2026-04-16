ea583caf207a039a8b7a25e5a2f806fef63d5d85
Standa Musil <standa@nimbus.co>
2026-04-16 13:39:34 -0700

    prepare for 0.0.1 placeholder release

---
+++ Sources/NimbusMintegralKit/MintegralExtension.swift
@@ -0,0 +1,8 @@
+//
+//  MintegralExtension.swift
+//  Nimbus
+//  Created on 4/1/25
+//  Copyright © 2025 Nimbus Advertising Solutions Inc. All rights reserved.
+//
+
+public struct MintegralExtension {}
